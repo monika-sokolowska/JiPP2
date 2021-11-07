4644588c8412270df667cb3085a346f0756d0904
cynamon <monika.sokolowska@student.pk.edu.pl>
2021-11-07 20:46:01 +0100

    Class_lab4 _ Ćw.2 + gettery,settery

--- Class2_lab4/src/main.cpp
+++ Class2_lab4/src/main.cpp
@@ -8,14 +8,14 @@ using namespace std;
 
 int main()
 {
-    unsigned int a;
+    unsigned int r;
 
     Kula sphere;
 
     cout << "Podaj promien kuli" << endl;
-    cin >> a;
+    cin >> r;
 
-    sphere.setRadius(a);
+    sphere.setRadius(r);
     sphere.countVolume();
     sphere.printVolume();
 
